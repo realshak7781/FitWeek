ebf806231c894e0b1fc6b1858437767fe6e26b7e
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2026-02-16 18:55:46 +0530

    feat: implement WorkoutSession and SetLog entities with JpaRepositories

--- backend-spring/src/main/java/com/fitweek/backend/domain/entities/WorkoutSession.java
+++ backend-spring/src/main/java/com/fitweek/backend/domain/entities/WorkoutSession.java
@@ -1,32 +1,32 @@
-//package com.fitweek.backend.domain.entities;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Entity
-//@Table(name = "workout_sessions")
-//@Getter @Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class WorkoutSession {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private LocalDate workoutDate;
-////    this is the name of the type of the workout
-//    private String title;
-//
-//    @Column(columnDefinition = "TEXT")
-//    private String aiCoachNotes;
-//
-////    now this contains a list of workout sessions
-//    @OneToMany(mappedBy = "session", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<SetLog> setLogs=new ArrayList<>();
-//}
+package com.fitweek.backend.domain.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "workout_sessions")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkoutSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate workoutDate;
+//    this is the name of the type of the workout
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String aiCoachNotes;
+
+//    now this contains a list of workout sessions
+    @OneToMany(mappedBy = "session", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SetLog> setLogs=new ArrayList<>();
+}
